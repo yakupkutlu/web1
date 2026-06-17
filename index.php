774227dcb5f62f8e5e0368a8bf98c536c687ea79
DESKTOP-J78FU8R\Acer <yakupkutlu@gmail.com>
2026-06-17 23:58:46 +0300

    index

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-$conn = new mysqli(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASSWORD"), getenv("DB_NAME"));
-if ($conn->connect_error) {
-    die("Bağlantı hatası: " . $conn->connect_error);
-}
-echo "MySQL bağlantısı başarılı!";
+
+
+include("header.php");
+include("main.php");
+include("footer.php");
+
 ?>
